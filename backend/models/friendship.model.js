a73221f66639c9fe0f93daca516a8c4a13dd2515
Stefany Coromac <stefanycoromac.23@gmail.com>
2021-10-20 02:22:39 -0600

    UPDATE DDL

--- backend/models/friendship.model.js
+++ backend/models/friendship.model.js
@@ -23,23 +23,57 @@ const friendshipModel = {
             username
         } = params; 
 
-        let query = `SELECT a.usuario FROM AMISTAD a
-        WHERE amigo =  '${username}' AND estado = 1; `
+        let query = `SELECT a.usuario AS username FROM AMISTAD a
+        WHERE amigo =  '${username}' AND estado = 1 ; `
 
         return this.executeQuery(query, callback)
     }, 
 
     update(params, callback){
+        const {
+            username, 
+            friend
+        } = params;
+
+        let query = `UPDATE AMISTAD
+        SET estado = 2 
+        WHERE usuario = ${username} AND amigo = '${friend}'`
         
+        return this.executeQuery(query, callback)
     }, 
 
+    delete(params, callback){
+        const {
+            username, 
+            friend
+        } = params;
+
+        let query = `DELETE FROM USUARIO 
+        WHERE usuario = ${username} AND amigo = '${friend}'`
+        
+        return this.executeQuery(query, callback)
+    }, 
+
+    getFriends(params, callback){
+        const {
+            username
+        } = params; 
+
+        let query = `(select username, nombre as name, foto as profile_picture, modobot as bot_mode  from USUARIO u, AMISTAD a
+            where a.usuario = '${username}' and a.amigo = u.username and a.estado = 1)
+            UNION
+            (select username, nombre as name, foto as profile_picture, modobot as bot_mode  from USUARIO u, AMISTAD a
+            where a.usuario = u.username and a.amigo = '${username}'  and a.estado = 1);`
+
+        return this.executeQuery(query, callback)
+    }, 
 
     getNoFriend(params, callback){
         const {
             username
         } = params; 
 
-        let query = `SELECT u.* FROM USUARIO u
+        let query = `SELECT u.username, u.nombre AS name, u.foto AS profile_picture, u.modobot AS bot_mode FROM USUARIO u
         WHERE u.username NOT IN (SELECT a.amigo FROM AMISTAD a 
         WHERE a.usuario = '${username}' UNION  SELECT a.usuario FROM AMISTAD a 
         WHERE a.amigo = '${username}') AND u.username !=  '${username}';`
