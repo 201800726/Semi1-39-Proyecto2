f69cac83b0b1c9d16ce70843ec253cd64a52756c
201800726 <57029729+201800726@users.noreply.github.com>
2021-10-19 19:15:23 -0600

    [ADD] get friends route

--- backend/models/friendship.model.js
+++ backend/models/friendship.model.js
@@ -1,36 +1,42 @@
-const db = require('../database/database')
+const db = require("../database/database");
 
 const friendshipModel = {
-    executeQuery(query, callback) {
-        db.query(query, (err, res) => callback(err, res));
-    },
+  executeQuery(query, callback) {
+    db.query(query, (err, res) => callback(err, res));
+  },
 
-    sendFriendRequest(params, callback) {
-        const {
-            user,
-            friend
-        } = params
+  sendFriendRequest(params, callback) {
+    const { user, friend } = params;
 
-        let query = `INSERT INTO SOLICITUD (usuario, amigo, estado) 
-        VALUES ('${user}', '${friend}', false)`
+    let query = `INSERT INTO SOLICITUD (usuario, amigo, estado) 
+        VALUES ('${user}', '${friend}', false)`;
 
-        return this.executeQuery(query, callback)
-    },
+    return this.executeQuery(query, callback);
+  },
 
-    create(params, callback) {
-        const {
-            user,
-            friend
-        } = params;
+  create(params, callback) {
+    const { user, friend } = params;
 
-        let query = `INSERT INTO AMISTAD (usuario, amigo) 
+    let query = `INSERT INTO AMISTAD (usuario, amigo) 
         VALUES ('${user}', '${friend}'); 
 
         INSERT INTO AMISTAD (usuario, amigo) 
-        VALUES ('${friend}', '${user}' ); `
+        VALUES ('${friend}', '${user}' ); `;
 
-        return this.executeQuery(query, callback)
-    }
-}
+    return this.executeQuery(query, callback);
+  },
+
+  getFriends(params, callback) {
+    const { username } = params;
+
+    let query = ` (select username, nombre as name, foto as profile_picture, modobot as bot_mode  from USUARIO u, AMISTAD a
+            where a.usuario = '${username}' and a.amigo = u.username and a.estado = 1)
+            UNION
+            (select username, nombre as name, foto as profile_picture, modobot as bot_mode  from USUARIO u, AMISTAD a
+            where a.usuario = u.username and a.amigo = '${username}'  and a.estado = 1);`;
+
+    return this.executeQuery(query, callback);
+  },
+};
 
 module.exports = friendshipModel;
--- backend/routes/friendship.route.js
+++ backend/routes/friendship.route.js
@@ -1,9 +1,9 @@
-const friendshipController = require('../controllers/friendship.controller')
-const express = require('express')
-const router = express.Router()
+const friendshipController = require("../controllers/friendship.controller");
+const express = require("express");
+const router = express.Router();
 
-
-router.post('/sendRequest', friendshipController.sendFriendRequest);
-router.post('/create', friendshipController.create);
+router.post("/sendRequest", friendshipController.sendFriendRequest);
+router.post("/create", friendshipController.create);
+router.post("/friends", friendshipController.getFriends);
 
 module.exports = router;
