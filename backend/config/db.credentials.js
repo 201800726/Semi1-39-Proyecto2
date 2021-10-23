68ec52d0f88dfd7fb3f0c5535de40d8f42fb90a3
Ubuntu <ubuntu@ip-172-31-42-3.us-east-2.compute.internal>
2021-10-23 05:55:49 +0000

    ADD docker-compose backend

--- backend/config/db.credentials.js
+++ backend/config/db.credentials.js
@@ -1,9 +1,9 @@
 let db_credentials = {
-    host: '18.216.171.156', 
+    host: '3.144.142.34', 
     user: 'root', 
     password: 'proyecto2', 
     database: 'proyecto2', 
-    port: 3306
+    port: 3306, 
 }
 
 module.exports = db_credentials
